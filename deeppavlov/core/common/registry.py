1650af7624161337f74ff8bc7dd603cef8c64d76
oserikov <srkvoa@gmail.com>
2021-05-14 15:00:25 +0300

    wip codegen for openapi integration

--- deeppavlov/core/common/registry.py
+++ deeppavlov/core/common/registry.py
@@ -18,8 +18,6 @@ from logging import getLogger
 from pathlib import Path
 
 from deeppavlov.core.common.errors import ConfigError
-from deeppavlov.core.common.modules import obj_from_str
-
 
 logger = getLogger(__name__)
 
@@ -33,7 +31,13 @@ else:
 
 def cls_from_str(name: str) -> type:
     """Returns a class object with the name given as a string."""
-    return obj_from_str(name, "class", "ClassName")
+    try:
+        module_name, cls_name = name.split(':')
+    except ValueError:
+        raise ConfigError('Expected class description in a `module.submodules:ClassName` form, but got `{}`'
+                          .format(name))
+
+    return getattr(importlib.import_module(module_name), cls_name)
 
 
 def register(name: str = None) -> type:
